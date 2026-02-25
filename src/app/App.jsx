4a4f8e5a51c3e60497ee099140df5aa702c3709b
tahir76270 <motahir76270>
2026-02-25 22:58:08 +0530

    minin change

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -14,11 +14,12 @@
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
-import { store } from "./store";
+import { store } from "./redux/store";
 
 export default function App() {
   return (
     <Provider store={store}>
+      <h1>jdbjsdbh</h1>
       <RouterProvider router={router} />
     </Provider>
   );
